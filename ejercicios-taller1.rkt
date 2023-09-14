f301eae67ea9ba6dce6ba9edc7006ba2e8e2f892
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2023-09-14 11:19:18 -0500

    Ejercicio 3

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -1,3 +1,26 @@
-;;Hernan David Cisneros Vargas 2178192
-;;John Freddy Belalcar Rojas 2182464
-;;Julián David Rendon Cardona 2177387
+#lang eopl
+
+;; Hernan David Cisneros Vargas 2178192
+;; John Freddy Belalcar Rojas 2182464
+;; Julián David Rendon Cardona 2177387
+
+;--------------------------------------------------------------
+;--------------------------------------------------------------
+;; (3)
+;; list-set:
+;; Proposito:
+;; Lista x Int x Element -> Lista : Reemplaza el elemento n de la lista L por x.
+;;
+;; <Lista> := ()
+;;         := (<valor-de-scheme> <Lista>)
+
+(define list-set
+   (lambda (L n x)
+      (if (zero? n)
+         (cons x (cdr L))
+         (cons (car L) (list-set (cdr L) (- n 1) x)))))
+
+;; Pruebas
+
+(list-set '(a b c d) 2 '(1 2))
+(list-set '(a b c d) 3 '(1 5 10))
