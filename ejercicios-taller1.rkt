ee7e91e047b95a9b5287ca3fc602915fe63a8d54
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2023-09-14 23:55:06 -0500

    Copie lo de julian

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -244,6 +244,286 @@
                            multiplica
                            ((2 multiplica 4) resta 1))) ; Deberia imprimir 70
 
+;-------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------
+;;(2)
+;;down:
+;;Propósito: Función que recibe como único argumento un lista L y retorna
+;;una lista con cada uno de los elementos de L con un nivel más de paréntesis
+;;comparado con el que tenía incialmente.
+;;
+;;List -> List
+;;
+;;Gramática:
+;;
+;; <S-list> ::= ({<S-exp>}*)
+;; <S-exp>  ::= <Symbol> | <S-list>
+;;
+
+(define down
+  (lambda (L)
+    (if(null? L)
+       '()
+       (cons(cons(car L)empty)(down (cdr L))))))
+
+;; Pruebas "down"
+
+(define d1 (down '(1 2 3)))
+(define d2 (down '((una) (buena) (idea))))
+(define d3 (down '(a (23) (((527))))))
+(define d4 (down '(un (objeto (mas)) complicado)))
+
+;-------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------
+;;(5)
+;;list-index:
+;;Propósito: Función que recibe dos argumentos un predicado P y una lista L y
+;;lo que retorna es la posición del primer elemento que satisface el predicado dado.
+;;Si ningún elemento satisface dicho predicado, se retorna #f. 
+;;
+;;P x L -> Int
+;;
+;;
+;;Función auxiliar aux-index:
+;;Propósito: Función que recibe tres argumentos, donde dos de ellos son los mismos
+;;de la función principal list-index y el otro es un entero que representa el index
+;;de la lista, el cual se inicializa en 0 y se llama recursivamente hasta encontrar
+;;el elemento que cumple el predicado.
+;;
+;;P x L x Int -> Int
+;;
+;;Gramática:
+;;
+;;<S-list> ::= ({<S-exp>}*)
+;;<S-exp>  ::= <Symbol> | <S-list>
+;;
+
+(define list-index
+  (lambda (P L)
+
+    ;;Función auxiliar que me permite adquirir el indice de la lista
+    (define aux-index
+      (lambda (P L I)
+        (cond
+          ((null? L) #f)
+          ((P (car L)) I)
+          (else (aux-index P (cdr L) (+ 1 I))))))
+
+    (aux-index P L 0)))
+
+;;Pruebas "list-index"
+
+(define LI1 (list-index number? '(a 2 (1 3) b 7)))
+(define LI2 (list-index symbol? '(a 2 (1 3) b 7)))
+(define LI3 (list-index list? '(a 2 (1 3) b 7)))
+(define LI4 (list-index symbol? '(2 (1 a 3 f) 7)))
+
+
+;-------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------
+;;(8)
+;;mapping:
+;;Propósito: Función que recibe tres argumentos, una función F que recibe un argumento,
+;;y dos listas L1 y L2 donde ambas son de igual tamaño. La función debe retornar una lista de pares (a,b) 
+;;donde a es elemento de L1 y b es elemento de L2, y donde se debe cumplir que F(a) = b.
+;;
+;;Function x List x List -> List(List)
+;;
+;;Función auxiliar cumple?:
+;;Propósito: Función que recibe dos argumentos el cual me sirve para representar F(a) = b
+;;y poder validar si se cumple el requisito en la función pares.
+;;
+;;Función auxiliar pares:
+;;Propósito: Función que recibe dos listas, y el cual retorna una lista con los elementos que
+;;cumplen con F(a) = b, donde a es un elemento de lista 1 y b un elemento de la lista 2.
+;;
+;;Gramática:
+;;
+;;<S-list> ::= ({<S-exp>}*)
+;;<S-exp>  ::= <Symbol> | <S-list>
+;;
+
+(define mapping
+  (lambda (F L1 L2)
+
+    ;;Función que me verifica si se cumple F(a) = b
+    (define cumple?
+      (lambda (a b)
+        (= (F a) b)))
+   
+  ;; Función auxiliar para generar la lista de pares
+  (define pares
+    (lambda (lista1 lista2)
+    (cond
+      ((null? lista1) '())
+      ((not (= (length L1) (length L2))) "No son de igual tamaño")
+      (else
+       (if (cumple? (car lista1) (car lista2))
+           (cons (list (car lista1) (car lista2)) (pares (cdr lista1) (cdr lista2)))
+           (pares (cdr lista1) (cdr lista2)))))))
+
+  (pares L1 L2)))
+
+;;Pruebas "mapping"
+
+(define m1 (mapping (lambda (d) (* d 2)) (list 1 2 3) (list 2 4 6)))
+(define m2 (mapping (lambda (d) (* d 3)) (list 1 2 2) (list 2 4 6)))
+(define m3 (mapping (lambda (d) (* d 2)) (list 1 2 3) (list 3 9 12)))
+(define m4 (mapping (lambda (d) (- 4 d)) (list 1 2 3) (list 3 9 12)))
+
+;-------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------
+;;(11)
+;;zip:
+;;Propósito: Función que recibe tres argumentos, una función F que recibe dos argumentos,
+;;y dos listas L1 y L2 donde ambas son de igual tamaño. La función debe retornar una lista 
+;;donde la posición n-ésima corresponde al resultado de aplicar la función F sobre los elementos
+;;en la posición n-ésima de las listas L1 y L2.
+;;
+;;Function x List x List -> List
+;;
+;;Función auxiliar parametros-F:
+;;Propósito: Función que recibe los dos argumentos que necesita la función F.
+;;
+;;<lista-de-enteros> ::= ()
+;;                   ::= (<procedure> <lista-de-enteros> <lista-de-enteros>)
+;;<lista-de-enteros> ::= ()|(<int> <lista-de-enteros>)
+;;<lista-de-enteros> ::= ({<int>}*)
+
+
+
+(define zip
+  (lambda (F L1 L2)
+    ;;Función que recibe los dos parámetros de la función F
+    (define parametros-F
+      (lambda (a b)
+        (F a b)))
+    
+    (cond
+      ((null? L1) '())
+      ((not(=(length L1)(length L2))) "No son de igual tamaño")
+      (else
+       (cons (parametros-F (car L1) (car L2))
+             (zip F (cdr L1) (cdr L2)))))))
+    
+  
+
+;;Pruebas "zip"
+
+(define z1 (zip + '(1 4) '(6 2)))
+(define z2 (zip * '(11 5 6) '(10 9 8)))
+(define z3 (zip - '(30 12 21) '(7 5 14)))
+(define z4 (zip / '(63 55 28) '(9 11 7)))
+
+;-------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------
+;;(14)
+;;path:
+;;Propósito: Función que recibe dos argumentos, un número n y un árbol binario de búsqueda BST.
+;;La función debe retornar una lista con la ruta a tomar indicada por cadenas de left y right,
+;;hasta llegar al número n. Si n se encuentra en el nodo raíz se retorna una lista vacía.
+;;
+;;Int x arbol-binario -> List
+;;
+;;Función auxiliar hallar-camino:
+;;Propósito: Función que recibe dos listas donde una representa el camino del árbol por donde va
+;;el recorrido y la otra representa el nodo en el cual está analizando la función para avanzar, al final
+;;se retorna la lista con el camino a seguir.
+;;
+;;< ́arbol-binario> := ()
+;;                 := Int <́arbol-binario> <́arbol-binario>
+
+(define path
+  (lambda (n BST)
+
+    ;;Función que me une los elementos de varias listas en una sola lista
+    (define mi_append
+      (lambda (L1 L2)
+        (if (null? L1)
+            L2
+            (cons (car L1) (mi_append (cdr L1) L2)))))
+    
+  ; Función auxiliar para encontrar la ruta desde el nodo actual hasta n
+  (define hallar-camino
+    (lambda (camino-actual nodo-actual)
+    (cond
+      ((null? nodo-actual) '())
+      ((= n (car nodo-actual)) camino-actual)
+      ((< n (car nodo-actual))
+       (hallar-camino (mi_append camino-actual '(left)) (cadr nodo-actual)))
+      ((> n (car nodo-actual))
+       (hallar-camino (mi_append camino-actual '(right)) (caddr nodo-actual)))
+      (else '()))))
+  
+  (hallar-camino '() BST)))
+
+;;Pruebas "path"
+
+(define p1 (path 17 '(14 (7 () (12 () ()))
+                          (26 (20 (17 () ())())
+                              (31 () ())))))
+
+(define p2 (path 31 '(14 (7 () (12 () ()))
+                          (26 (20 (17 () ())())
+                              (31 () ())))))
+
+(define p3 (path 26 '(14 (7 () (12 () ()))
+                          (26 (20 (17 () ())())
+                              (31 () ())))))
+
+(define p4 (path 11 '(11 (7 () (12 () ()))
+                          (26 (20 (17 () ())())
+                              (31 () ())))))
+
+
+;-------------------------------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------
+;;(17)
+;;prod-scalar-matriz:
+;;Propósito: Función que recibe una matriz mat representada como una lista de listas y
+;;un vector vec representado como una lista y retorna como resultado la multiplicación 
+;;de matriz por vector.
+;;
+;;List(List) x List -> List(List)
+;;
+;;Función auxiliar prod-vector:
+;;Propósito: Función que recibe dos listas que representan dos vectores y la cual retorna
+;;una lista con el producto vectorial entre dichos vectores, sirve para hacer el producto
+;;de la matriz obteniendo primero uno de los vectores de dicha matriz y multiplicandolo con
+;;el vector de entrada.
+;;
+;;Gramática:
+;;
+;;<enteros> ::= ()
+;;          ::= (<int> <enteros>)
+;;<enteros> ::= ()|((<int>) <enteros>)
+;;<enteros> ::= ({<int>}*)
+;;
+
+(define prod-scalar-matriz
+  (lambda (mat vec)
+
+    ;;Función que me multiplica los vectores de la matriz con el vector 
+    (define prod-vector
+      (lambda (vec1 vec2)
+        (if (null? vec1)
+          '()
+          (cons (* (car vec1) (car vec2))
+          (prod-vector (cdr vec1) (cdr vec2))))))
+
+    (if (null? mat)
+        '()
+        (cons(prod-vector(car mat) vec)
+             (prod-scalar-matriz (cdr mat) vec)))))
+
+
+;;Pruebas "prod-scalar-matriz"
+
+(define psm1 (prod-scalar-matriz '((1 1) (2 2)) '(2 3)))
+(define psm2 (prod-scalar-matriz '((1 1) (2 2) (3 3)) '(2 3)))
+(define psm3 (prod-scalar-matriz '((2 15 6 1) (2 7 4 11) (3 3 6 9)) '(10 2 5 7)))
+(define psm4 (prod-scalar-matriz '((2 15 6 1 6) (2 7 4 11 2) (3 3 6 9 2) (5 5 8 4 2)) '(2 5 3 9 8)))
+
 ;--------------------------------------------------------------
 ;--------------------------------------------------------------
 ;; (3)
